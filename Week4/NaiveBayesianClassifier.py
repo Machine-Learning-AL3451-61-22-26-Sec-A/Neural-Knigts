2555ee2639a08d018c026dd7d32a97a40080c606
dharaneesh-21 <163978108+dharaneesh-21@users.noreply.github.com>
2024-05-20 20:01:54 +0530

    Update NaiveBayesianClassifier.py

--- Week4/NaiveBayesianClassifier.py
+++ Week4/NaiveBayesianClassifier.py
@@ -1,45 +1,71 @@
+import os
+import subprocess
+import sys
 
-# import necessary libarities
-import pandas as pd
-from sklearn import tree
-from sklearn.preprocessing import LabelEncoder
-from sklearn.naive_bayes import GaussianNB
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-# load data from CSV
-data = pd.read_csv('trainingdata.csv')
-print("THe first 5 values of data is :\n",data.head())
+try:
+    import pandas as pd
+except ModuleNotFoundError:
+    install('pandas')
+    import pandas as pd
 
-# obtain Train data and Train output
-X = data.iloc[:,:-1]
-print("\nThe First 5 values of train data is\n",X.head())
+try:
+    from sklearn import tree
+    from sklearn.preprocessing import LabelEncoder
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import accuracy_score
+except ModuleNotFoundError:
+    install('scikit-learn')
+    from sklearn import tree
+    from sklearn.preprocessing import LabelEncoder
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import accuracy_score
 
-y = data.iloc[:,-1]
-print("\nThe first 5 values of Train output is\n",y.head())
+# Load data from CSV
+try:
+    data = pd.read_csv('trainingdata.csv')
+    print("The first 5 values of data:\n", data.head())
+except FileNotFoundError:
+    print("File not found. Please ensure the 'trainingdata.csv' file is in the correct path.")
+    exit()
 
-# Convert then in numbers 
+# Obtain Train data and Train output
+X = data.iloc[:, :-1]
+print("\nThe first 5 values of train data:\n", X.head())
+
+y = data.iloc[:, -1]
+print("\nThe first 5 values of train output:\n", y.head())
+
+# Convert categorical data to numbers
 le_outlook = LabelEncoder()
-X.Outlook = le_outlook.fit_transform(X.Outlook)
+X['Outlook'] = le_outlook.fit_transform(X['Outlook'])
 
 le_Temperature = LabelEncoder()
-X.Temperature = le_Temperature.fit_transform(X.Temperature)
+X['Temperature'] = le_Temperature.fit_transform(X['Temperature'])
 
 le_Humidity = LabelEncoder()
-X.Humidity = le_Humidity.fit_transform(X.Humidity)
+X['Humidity'] = le_Humidity.fit_transform(X['Humidity'])
 
 le_Windy = LabelEncoder()
-X.Windy = le_Windy.fit_transform(X.Windy)
+X['Windy'] = le_Windy.fit_transform(X['Windy'])
 
-print("\nNow the Train data is :\n",X.head())
+print("\nNow the train data is:\n", X.head())
 
 le_PlayTennis = LabelEncoder()
 y = le_PlayTennis.fit_transform(y)
-print("\nNow the Train output is\n",y)
+print("\nNow the train output is:\n", y)
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.20)
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 
+# Train the classifier
 classifier = GaussianNB()
-classifier.fit(X_train,y_train)
+classifier.fit(X_train, y_train)
 
-from sklearn.metrics import accuracy_score
-print("Accuracy is:",accuracy_score(classifier.predict(X_test),y_test))
+# Evaluate the classifier
+accuracy = accuracy_score(classifier.predict(X_test), y_test)
+print("Accuracy is:", accuracy)
